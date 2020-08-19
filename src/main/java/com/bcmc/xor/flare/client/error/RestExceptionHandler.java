4d353579c89b0c8d428120a8201a7de6d5d3ef3c
Li, William <william.li@us-dde.net>
2020-08-19 09:38:37 -0400

    Merge Jeff G from CTMART-1765_Server_API : RestExceptionHandler.java APIError.java APISubError.java in

---
+++ src/main/java/com/bcmc/xor/flare/client/error/RestExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.bcmc.xor.flare.client.error;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class RestExceptionHandler  extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(BadCredentialsException.class)
+    protected ResponseEntity<Object> handleBadCredentialsException(BadCredentialsException ex) {
+        APIError apiError = new APIError(HttpStatus.UNAUTHORIZED);
+        apiError.setMessage(ex.getMessage());
+        return buildResponseEntity(apiError);
+    }
+
+    private ResponseEntity<Object> buildResponseEntity(APIError apiError) {
+        return new ResponseEntity<>(apiError, apiError.getHttpStatus());
+    }
+}
