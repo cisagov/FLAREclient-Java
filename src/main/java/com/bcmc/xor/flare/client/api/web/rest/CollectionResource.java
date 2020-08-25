4a79c7a69c023789966e16601efc5de710d7395b
jeff.grosso <jeff.grosso@us-dde.net>
2020-08-25 15:09:21 -0400

    JLG: CTMART-1759 Collection API error handling. Increase unit test coverage for Server API to over 80%.

--- src/main/java/com/bcmc/xor/flare/client/api/web/rest/CollectionResource.java
+++ src/main/java/com/bcmc/xor/flare/client/api/web/rest/CollectionResource.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
-@SuppressWarnings("unused")
 @RestController
 @RequestMapping("/api/servers/{serverLabel}/collections")
 class CollectionResource {
