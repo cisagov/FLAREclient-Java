6e348d6ae965f6106e18942ecb4a698cd74bc732
Li, William <william.li@us-dde.net>
2020-08-26 12:15:59 -0400

    WLI: Add error handling for user authentication status check.

--- src/main/java/com/bcmc/xor/flare/client/api/web/rest/AccountResource.java
+++ src/main/java/com/bcmc/xor/flare/client/api/web/rest/AccountResource.java
@@ -31,20 +31,16 @@ import java.util.Optional;
 @RequestMapping("/api")
 public class AccountResource {
 
-    private static final Logger log = LoggerFactory.getLogger(AccountResource.class);
+	private static final Logger log = LoggerFactory.getLogger(AccountResource.class);
+	private final UserRepository userRepository;
+	private final UserService userService;
+	private final MailService mailService;
 
-    private final UserRepository userRepository;
-
-    private final UserService userService;
-
-    private final MailService mailService;
-
-    public AccountResource(UserRepository userRepository, UserService userService, MailService mailService) {
-
-        this.userRepository = userRepository;
-        this.userService = userService;
-        this.mailService = mailService;
-    }
+	public AccountResource(UserRepository userRepository, UserService userService, MailService mailService) {
+		this.userRepository = userRepository;
+		this.userService = userService;
+		this.mailService = mailService;
+	}
 
 	/**
 	 * POST /register : register the user.
@@ -66,10 +62,12 @@ public class AccountResource {
 		User user = new User();
 
 		userRepository.findOneByLogin(managedUserVM.getLogin().toLowerCase()).ifPresent(u -> {
+			log.error("REST API AccountResource register: Exception: LoginAlreadyUsedException ");
 			throw new LoginAlreadyUsedException();
 		});
 
 		userRepository.findOneByEmailIgnoreCase(managedUserVM.getEmail()).ifPresent(u -> {
+			log.error("REST API AccountResource register: Exception: EmailAlreadyUsedException ");
 			throw new EmailAlreadyUsedException();
 		});
 
@@ -80,19 +78,19 @@ public class AccountResource {
 		return new ResponseEntity<>(user, httpHeaders, HttpStatus.CREATED);
 	}
 
-//	@ExceptionHandler(LoginAlreadyUsedException.class)
-//	public ResponseEntity<Exception> handleLoginAlreadyUsedException() {
-//		HttpHeaders httpHeaders = new HttpHeaders();
-//		httpHeaders.add("api-register", ErrorConstants.ERR_LOGIN_IN_USED);
-//		return new ResponseEntity<>(new LoginAlreadyUsedException(), httpHeaders, HttpStatus.CONFLICT);
-//	}
-//
-//	@ExceptionHandler(EmailAlreadyUsedException.class)
-//	public ResponseEntity<Object> handleEmailAlreadyUsedException() {
-//		HttpHeaders httpHeaders = new HttpHeaders();
-//		httpHeaders.add("api-register", ErrorConstants.ERR_EMAIL_IN_USED);
-//		return new ResponseEntity<>(new EmailAlreadyUsedException(), httpHeaders, HttpStatus.CONFLICT);
-//	}
+	@ExceptionHandler(LoginAlreadyUsedException.class)
+	public ResponseEntity<Exception> handleLoginAlreadyUsedException() {
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.add("api-register", ErrorConstants.ERR_LOGIN_IN_USED);
+		return new ResponseEntity<>(new LoginAlreadyUsedException(), httpHeaders, HttpStatus.CONFLICT);
+	}
+
+	@ExceptionHandler(EmailAlreadyUsedException.class)
+	public ResponseEntity<Object> handleEmailAlreadyUsedException() {
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.add("api-register", ErrorConstants.ERR_EMAIL_IN_USED);
+		return new ResponseEntity<>(new EmailAlreadyUsedException(), httpHeaders, HttpStatus.CONFLICT);
+	}
 
 	/**
 	 * GET /activate : activate the registered user.
@@ -109,6 +107,7 @@ public class AccountResource {
 		HttpHeaders httpHeaders = new HttpHeaders();
 		Optional<User> user = userService.activateRegistration(key);
 		if (!user.isPresent()) {
+			log.error("REST API AccountResource activate: Exception: AccountActivationException ");
 			throw new AccountActivationException();
 		}
 
@@ -122,33 +121,59 @@ public class AccountResource {
 		httpHeaders.add("api-register", ErrorConstants.ERR_ACTIVATIONKEY_NOT_FOUND);
 		return new ResponseEntity<>(new AccountActivationException(), httpHeaders, HttpStatus.BAD_REQUEST);
 	}
-	
-    /**
-     * GET  /authenticate : check if the user is authenticated, and return its login.
-     *
-     * @param request the HTTP request
-     * @return the login if the user is authenticated
-     */
-    @GetMapping("/authenticate")
-    @Timed
-    public String isAuthenticated(HttpServletRequest request) {
-        log.debug("REST request to check if the current user {} is authenticated", request.getRemoteUser());
-        return request.getRemoteUser();
-    }
 
-    /**
-     * GET  /account : get the current user.
-     *
-     * @return the current user
-     * @throws RuntimeException 500 (Internal Server Error) if the user couldn't be returned
-     */
-    @GetMapping("/account")
-    @Timed
-    public UserDTO getAccount() {
-        return userService.getUserWithAuthorities()
-            .map(UserDTO::new)
-            .orElseThrow(() -> new NotFoundException("User could not be found"));
-    }
+	/**
+	 * GET /authenticate : check if the user is authenticated, and return its login.
+	 *
+	 * @param request the HTTP request
+	 * @return the login if the user is authenticated
+	 */
+	@GetMapping("/authenticate")
+	@Timed
+	@ResponseBody
+	public Object isAuthenticated(HttpServletRequest request) {
+		try {
+			HttpHeaders httpHeaders = new HttpHeaders();
+			String login = request.getRemoteUser();
+
+			if (login.isEmpty() || login == null) {
+				httpHeaders.add("api-get-authenticate", "error");
+				log.error("AccountResource : isAuthenticated : error : user not found");
+				return new ResponseEntity<>(new UserNotFoundException(), httpHeaders, HttpStatus.NOT_FOUND);
+			}
+
+			httpHeaders.add("api-get-authenticate", "ok");
+			log.debug("REST request to check if the current user is authenticated");
+			Object o = userService.getUserWithAuthorities().map(UserDTO::new)
+					.orElseThrow(() -> new NotFoundException("User could not be found"));
+
+			return new ResponseEntity<>(o, httpHeaders, HttpStatus.ACCEPTED);
+		} catch (Exception e) {
+			log.error("AccountResouce : isAuthenticated : error : UserNotFoundException");
+			throw new UserNotFoundException();
+		}
+	}
+
+	@ExceptionHandler(UserNotFoundException.class)
+	public ResponseEntity<Object> handleUserNotFoundException() {
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.add("api-get-authenticate", ErrorConstants.ERR_USER_NOT_FOUND);
+		return new ResponseEntity<>(new UserNotFoundException(), httpHeaders, HttpStatus.NOT_FOUND);
+	}
+
+	/**
+	 * GET /account : get the current user.
+	 *
+	 * @return the current user
+	 * @throws RuntimeException 500 (Internal Server Error) if the user couldn't be
+	 *                          returned
+	 */
+	@GetMapping("/account")
+	@Timed
+	public UserDTO getAccount() {
+		return userService.getUserWithAuthorities().map(UserDTO::new)
+				.orElseThrow(() -> new NotFoundException("User could not be found"));
+	}
 
 	/**
 	 * POST /account : update the current user information.
@@ -156,7 +181,7 @@ public class AccountResource {
 	 * @param userDTO the current user information
 	 * @throws LoginAlreadyUsedException 409 if the user try to change email
 	 * @throws EmailAlreadyUsedException 409 if the email is already used
-	 * @throws AccountUpdateException          400 (Bad Request) Other exceptions 
+	 * @throws AccountUpdateException    400 (Bad Request) Other exceptions
 	 */
 	@PostMapping("/account")
 	@Timed
@@ -174,11 +199,13 @@ public class AccountResource {
 			if (existingUser.isPresent() && (!existingUser.get().getLogin().equalsIgnoreCase(userLogin))
 					|| !userDTO.getLogin().equals(userLogin)) {
 				httpHeaders.add("api-account-update", ErrorConstants.ERR_EMAIL_IN_USED);
+				log.error("REST API AccountResource account update: Exception: EmaaaaailAlreadyUsedException ");
 				return new ResponseEntity<>(new EmailAlreadyUsedException(), httpHeaders, HttpStatus.CONFLICT);
 			}
 
 			if (!user.isPresent()) {
 				httpHeaders.add("api-account-update", ErrorConstants.ERR_BAD_REQUEST);
+				log.error("REST API AccountResource account update: Exception: AccountUpdateException ");
 				return new ResponseEntity<>(new AccountUpdateException(), httpHeaders, HttpStatus.BAD_REQUEST);
 			}
 
@@ -187,8 +214,9 @@ public class AccountResource {
 
 			httpHeaders.add("api-account-update", "accepted");
 			return new ResponseEntity<>(user, httpHeaders, HttpStatus.ACCEPTED);
-			
+
 		} catch (Exception e) {
+			log.error("REST API AccountResource account update: Exception: AccountUpdaeException ");
 			throw new AccountUpdateException();
 		}
 	}
@@ -204,7 +232,8 @@ public class AccountResource {
 	 * POST /account/change-password : changes the current user's password
 	 *
 	 * @param passwordChangeDto current and new password
-	 * @throws InvalidPasswordException 400 (Bad Request) if the new password is incorrect
+	 * @throws InvalidPasswordException 400 (Bad Request) if the new password is
+	 *                                  incorrect
 	 *
 	 */
 	@PostMapping(path = "/account/change-password")
@@ -213,16 +242,18 @@ public class AccountResource {
 	public Object changePassword(@RequestBody PasswordChangeDTO passwordChangeDto) {
 		final String userLogin = SecurityUtils.getCurrentUserLogin()
 				.orElseThrow(() -> new InternalServerErrorException("Current user login not found"));
-		
+
 		if (checkPasswordLength(passwordChangeDto.getNewPassword())) {
+			log.error("REST API AccountResource account-change-password: Exception: InvalidPasswordException");
 			throw new InvalidPasswordException();
 		}
 
 		userService.changePassword(passwordChangeDto.getCurrentPassword(), passwordChangeDto.getNewPassword());
-	
+
 		HttpHeaders httpHeaders = new HttpHeaders();
 		httpHeaders.add("api-account-change-password", "accepted");
-		return new ResponseEntity<>(userService.getUserWithAuthoritiesByLogin(userLogin), httpHeaders, HttpStatus.ACCEPTED);
+		return new ResponseEntity<>(userService.getUserWithAuthoritiesByLogin(userLogin), httpHeaders,
+				HttpStatus.ACCEPTED);
 	}
 
 	@ExceptionHandler(InvalidPasswordException.class)
@@ -231,46 +262,49 @@ public class AccountResource {
 		httpHeaders.add("api-account-change-passowrd", ErrorConstants.ERR_ACCOUNT_UPDATE);
 		return new ResponseEntity<>(new InvalidPasswordException(), httpHeaders, HttpStatus.BAD_REQUEST);
 	}
-	
-    /**
-     * POST   /account/reset-password/init : Send an email to reset the password of the user
-     *
-     * @param mail the mail of the user
-     * @throws EmailNotFoundException 400 (Bad Request) if the email address is not registered
-     */
-    @PostMapping(path = "/account/reset-password/init")
-    @Timed
-    public void requestPasswordReset(@RequestBody String mail) {
-       mailService.sendPasswordResetMail(
-           userService.requestPasswordReset(mail)
-               .orElseThrow(EmailNotFoundException::new)
-       );
-    }
 
-    /**
-     * POST   /account/reset-password/finish : Finish to reset the password of the user
-     *
-     * @param keyAndPassword the generated key and the new password
-     * @throws InvalidPasswordException 400 (Bad Request) if the password is incorrect
-     * @throws RuntimeException 500 (Internal Server Error) if the password could not be reset
-     */
-    @PostMapping(path = "/account/reset-password/finish")
-    @Timed
-    public void finishPasswordReset(@RequestBody KeyAndPasswordVM keyAndPassword) {
-        if (checkPasswordLength(keyAndPassword.getNewPassword())) {
-            throw new InvalidPasswordException();
-        }
-        Optional<User> user =
-            userService.completePasswordReset(keyAndPassword.getNewPassword(), keyAndPassword.getKey());
+	/**
+	 * POST /account/reset-password/init : Send an email to reset the password of
+	 * the user
+	 *
+	 * @param mail the mail of the user
+	 * @throws EmailNotFoundException 400 (Bad Request) if the email address is not
+	 *                                registered
+	 */
+	@PostMapping(path = "/account/reset-password/init")
+	@Timed
+	public void requestPasswordReset(@RequestBody String mail) {
+		mailService
+				.sendPasswordResetMail(userService.requestPasswordReset(mail).orElseThrow(EmailNotFoundException::new));
+	}
 
-        if (!user.isPresent()) {
-            throw new NotFoundException("No user was found for this reset key");
-        }
-    }
+	/**
+	 * POST /account/reset-password/finish : Finish to reset the password of the
+	 * user
+	 *
+	 * @param keyAndPassword the generated key and the new password
+	 * @throws InvalidPasswordException 400 (Bad Request) if the password is
+	 *                                  incorrect
+	 * @throws RuntimeException         500 (Internal Server Error) if the password
+	 *                                  could not be reset
+	 */
+	@PostMapping(path = "/account/reset-password/finish")
+	@Timed
+	public void finishPasswordReset(@RequestBody KeyAndPasswordVM keyAndPassword) {
+		if (checkPasswordLength(keyAndPassword.getNewPassword())) {
+			throw new InvalidPasswordException();
+		}
+		Optional<User> user = userService.completePasswordReset(keyAndPassword.getNewPassword(),
+				keyAndPassword.getKey());
 
-    private static boolean checkPasswordLength(String password) {
-        return StringUtils.isEmpty(password) ||
-            password.length() < ManagedUserVM.PASSWORD_MIN_LENGTH ||
-            password.length() > ManagedUserVM.PASSWORD_MAX_LENGTH;
-    }
+		if (!user.isPresent()) {
+			log.error("REST API AccountResource account-reset-password User NotFoundException");
+			throw new NotFoundException("No user was found for this reset key");
+		}
+	}
+
+	private static boolean checkPasswordLength(String password) {
+		return StringUtils.isEmpty(password) || password.length() < ManagedUserVM.PASSWORD_MIN_LENGTH
+				|| password.length() > ManagedUserVM.PASSWORD_MAX_LENGTH;
+	}
 }
