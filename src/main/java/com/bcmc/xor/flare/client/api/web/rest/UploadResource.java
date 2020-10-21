89a502848a8372e5951dc84ffb8452464ecad0b0
jeff.grosso <jeff.grosso@us-dde.net>
2020-10-21 16:51:16 -0400

    JLG: CTMART-2164_Upload_Resource initial check-in for UI dev to be able to upload content

--- src/main/java/com/bcmc/xor/flare/client/api/web/rest/UploadResource.java
+++ src/main/java/com/bcmc/xor/flare/client/api/web/rest/UploadResource.java
@@ -5,14 +5,18 @@ import com.bcmc.xor.flare.client.api.service.CollectionService;
 import com.bcmc.xor.flare.client.api.service.ServerService;
 import com.bcmc.xor.flare.client.api.service.UploadService;
 import com.bcmc.xor.flare.client.taxii.TaxiiAssociation;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
-@SuppressWarnings("unused")
 @RestController
 @RequestMapping("/api/servers/{serverLabel}/collections/{collectionId}/upload")
 class UploadResource {
@@ -22,7 +26,6 @@ class UploadResource {
 
     private final ServerService serverService;
     private final CollectionService collectionService;
-
     private final UploadService uploadService;
 
     public UploadResource(ServerService serverService, CollectionService collectionService, UploadService uploadService) {
@@ -32,8 +35,21 @@ class UploadResource {
     }
 
     @PostMapping
-    public ResponseEntity<String> publish(@RequestBody Map<String, UploadedFile> fileMap, @PathVariable String serverLabel, @PathVariable String collectionId) {
+    public ResponseEntity<String> publish(@RequestParam("file") MultipartFile file, @RequestParam("filename") String filename, @PathVariable String serverLabel, @PathVariable String collectionId) throws IOException {
+        Map<String, UploadedFile> fileMap = new HashMap<>();
+        ByteArrayInputStream stream = new ByteArrayInputStream(file.getBytes());
+        String content = IOUtils.toString(stream);
+        UploadedFile  uploadedFile = new UploadedFile();
+        uploadedFile.setFilename(filename);
+        uploadedFile.setContent(content);
+        uploadedFile.setHash(1);
+        fileMap.put(filename,uploadedFile);
         TaxiiAssociation association = TaxiiAssociation.from(serverLabel, collectionId, serverService, collectionService);
         return uploadService.publish(association, fileMap);
     }
+//    @PostMapping
+//    public ResponseEntity<String> publish(@RequestBody Map<String, UploadedFile> fileMap, @PathVariable String serverLabel, @PathVariable String collectionId) {
+//        TaxiiAssociation association = TaxiiAssociation.from(serverLabel, collectionId, serverService, collectionService);
+//        return uploadService.publish(association, fileMap);
+//    }
 }
--- src/main/java/com/bcmc/xor/flare/client/taxii/taxii20/Taxii20RestTemplate.java
+++ src/main/java/com/bcmc/xor/flare/client/taxii/taxii20/Taxii20RestTemplate.java
@@ -93,9 +93,9 @@ public class Taxii20RestTemplate extends FlareRestTemplate {
     }
 
     public Status postBundle(Taxii20Server server, URI uri, String bundle) {
-        log.info("Attempting to POST content to '{}' with Content-Type: {}", uri.toString(), Constants.HEADER_STIX21_JSON_VERSION_21);
-        TaxiiHeaders headers = Taxii20Headers.fromServer(server).withHeader("Content-Type", Constants.HEADER_STIX21_JSON_VERSION_21);
-        ResponseEntity<String> responseEntity = executePost(bundle, headers, uri);
+        log.info("Attempting to POST content to '{}' with Content-Type: {}", uri.toString(), Constants.HEADER_STIX21_JSON_VERSION_21 +","+ Constants.HEADER_TAXII21_JSON_VERSION_21);
+        TaxiiHeaders headers = Taxii20Headers.fromServer(server).withHeader("Content-Type", Constants.HEADER_STIX21_JSON_VERSION_21 +","+ Constants.HEADER_TAXII21_JSON_VERSION_21);
+         ResponseEntity<String> responseEntity = executePost(bundle, headers, uri);
 
         if (responseEntity.getStatusCode().isError()) {
             TaxiiError taxiiError = JsonHandler.getInstance().fromJson(responseEntity.getBody(), TaxiiError.class);
