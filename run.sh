e33727114c4cf1ecab35e9a04aa20e82a2361dc5
tmai <trieu.mai@us-dde.net>
2020-10-23 13:51:26 -0400

    Add db user and password env variable

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
 docker rm -f flareclient
 docker run \
     --network ais20 \
+    -e FLARECLIENT_DB_USER="admin" \
+    -e FLARECLIENT_DB_PASS="QWASZX23wesdxc" \
     -p 8081:8081 \
     --name flareclient \
     flareclient
