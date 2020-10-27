aedd0e7ee81c409f8007451bf58eacaf674b7904
tmai <trieu.mai@us-dde.net>
2020-10-26 21:52:05 -0400

    Attempt to use image openjdk-11-rhel7

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ docker run \
     --network ais20 \
     -e FLARECLIENT_DB_USER="admin" \
     -e FLARECLIENT_DB_PASS="QWASZX23wesdxc" \
+    -e FLARECLIENT_DB_SVC_HOST="flareclient-db" \
+    -e FLARECLIENT_DB_SVC_PORT="27017" \
     -p 8083:8083 \
     --name flareclient \
     flareclient
