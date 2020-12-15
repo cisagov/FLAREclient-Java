87ca7002f9e4b7d7cd2fdfe5d72fb6d60d8d0135
jeff.grosso <jeff.grosso@us-dde.net>
2020-12-15 15:22:54 -0500

    JLG: CTMART-2701 - Remove unused imports

--- src/main/java/com/bcmc/xor/flare/client/api/web/rest/AuditResource.java
+++ src/main/java/com/bcmc/xor/flare/client/api/web/rest/AuditResource.java
@@ -14,10 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.Nullable;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
-import java.time.ZoneId;
 import java.util.List;
 
 /**
