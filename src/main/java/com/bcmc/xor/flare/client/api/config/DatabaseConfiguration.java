a41d85b6ba28f14365d08c0b06e5ab4f7dfb2cea
tmai <trieu.mai@us-dde.net>
2021-01-29 11:06:21 -0500

    Update to spring boot 2.3.8 and mongock

--- src/main/java/com/bcmc/xor/flare/client/api/config/DatabaseConfiguration.java
+++ src/main/java/com/bcmc/xor/flare/client/api/config/DatabaseConfiguration.java
@@ -1,28 +1,23 @@
 package com.bcmc.xor.flare.client.api.config;
 
 import com.bcmc.xor.flare.client.util.JSR310DateConverters;
-import com.github.mongobee.Mongobee;
-import com.mongodb.MongoClient;
+import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.SpringDataMongo3Driver;
+import com.github.cloudyrock.spring.v5.MongockSpring5;
+import com.github.cloudyrock.spring.v5.MongockSpring5.MongockInitializingBeanRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.convert.DbRefResolver;
-import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import java.util.ArrayList;
@@ -30,17 +25,12 @@ import java.util.List;
 
 @Configuration
 @EnableMongoRepositories("com.bcmc.xor.flare.client.api.repository")
+//@Profile("!" + JHipsterConstants.SPRING_PROFILE_CLOUD)
 @Import(value = MongoAutoConfiguration.class)
 @EnableMongoAuditing(auditorAwareRef = "springSecurityAuditorAware")
 public class DatabaseConfiguration {
 
     private static final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
-
-    @Autowired
-    MongoDbFactory mongoDbFactory;
-
-    @Autowired
-    MongoMappingContext mappingContext;
 
     @Bean
     public ValidatingMongoEventListener validatingMongoEventListener() {
@@ -53,24 +43,25 @@ public class DatabaseConfiguration {
     }
 
     @Bean
-    public Mongobee mongobee(MongoClient mongoClient, MongoTemplate mongoTemplate, MongoProperties mongoProperties) {
-        log.debug("Configuring Mongobee");
-        Mongobee mongobee = new Mongobee(mongoClient);
-        mongobee.setDbName(mongoProperties.getMongoClientDatabase());
-        mongobee.setMongoTemplate(mongoTemplate);
-        // package to scan for migrations
-        mongobee.setChangeLogsScanPackage("com.bcmc.xor.flare.client.api.config.dbmigrations");
-        mongobee.setEnabled(true);
-        return mongobee;
-    }
-
-    @Bean
-    public MappingMongoConverter mappingMongoConverter() throws Exception {
-        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDbFactory);
-        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mappingContext);
-        converter.setCustomConversions(customConversions());
-        converter.setMapKeyDotReplacement(",");
-        return converter;
+    public MongockInitializingBeanRunner mongockInitializingBeanRunner(
+            ApplicationContext springContext,
+            MongoTemplate mongoTemplate,
+            @Value("${mongock.lockAcquiredForMinutes:5}") long lockAcquiredForMinutes,
+            @Value("${mongock.maxWaitingForLockMinutes:3}") long maxWaitingForLockMinutes,
+            @Value("${mongock.maxTries:3}") int maxTries
+    ) {
+        SpringDataMongo3Driver driver = SpringDataMongo3Driver.withLockSetting(
+                mongoTemplate,
+                lockAcquiredForMinutes,
+                maxWaitingForLockMinutes,
+                maxTries
+        );
+        return MongockSpring5
+                .builder()
+                .setDriver(driver)
+                .addChangeLogsScanPackage("io.github.jhipster.sample.config.dbmigrations")
+                .setSpringContext(springContext)
+                .buildInitializingBeanRunner();
     }
 
     public MongoCustomConversions customConversions() {
